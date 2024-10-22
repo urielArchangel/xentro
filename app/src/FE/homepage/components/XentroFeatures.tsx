7dde566a5f2257799d98703195b5cd680ff2174d
Korex23 <ololokes@gmail.com>
2024-10-22 19:57:07 +0100

    built

--- app/src/FE/homepage/components/XentroFeatures.tsx
+++ app/src/FE/homepage/components/XentroFeatures.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 const XentroFeatures = () => {
   return (
     <>
-      <div className="py-16 px-20">
+      <div className="py-16 md:px-20 px-4">
         <div className="flex justify-center">
           <p className="text-white gilroy-bold text-3xl md:text-4xl lg:text-5xl mb-10 min-[1500px]:text-6xl">
             <span className="inline-block relative">
@@ -32,58 +32,72 @@ const XentroFeatures = () => {
             Features
           </p>
         </div>
-        <section className="grid grid-cols-7">
-          <div>
-            <Image src={bar} alt="mission" width={6.5} height={300} />
+        <section className="grid grid-cols-8 lg:grid-cols-7 items-stretch">
+          <div className="h-full">
+            <Image src={bar} alt="mission" width={7} height={300} />
           </div>
-          <div className="col-span-6">
-            <div className="grid grid-cols-6 mb-28">
+          <div className="col-span-7 lg:col-span-6 h-full">
+            <div className="grid grid-cols-7 md:grid-cols-6 md:mb-28">
               <FeatureCard
                 title="Decentralized Finance (DeFi) Integration"
                 description="Seamlessly Connect with the Future of Finance"
                 explanation="Access a wide range of DeFi services directly from our platform, allowing you to earn, borrow, and invest with unprecedented ease and security."
               />
-              <div className="col-span-2 ml-20">
-                <Image src={defi} alt="mission" width={270} height={300} />
+              <div className="col-span-7 lg:col-span-2 flex justify-center items-center md:ml-20 mt-10 lg:mt-0">
+                <Image
+                  src={defi}
+                  alt="mission"
+                  width={270}
+                  height={300}
+                  className="md:scale-[1] scale-[0.7]"
+                />
               </div>
             </div>
-            <div className="grid grid-cols-6 mb-28">
+            <div className="grid grid-cols-7 lg:grid-cols-6 mb-10 md:mb-28">
               <FeatureCard
                 title="Non-Custodial Management"
                 description="Retain Full Control Over Your Assets"
                 explanation="Access a curated selection of exclusive, secure investment opportunities with varying yield potentials, tailored to suit different risk profiles and financial goals."
               />
-              <div className="col-span-2 ml-20">
+              <div className="col-span-7 lg:col-span-2 flex justify-center items-center lg:ml-20 mt-10 lg:mt-0">
                 <Image
                   src={noncustodial}
                   alt="mission"
                   width={360}
                   height={300}
+                  className="md:scale-[1] scale-[0.7]"
                 />
               </div>
             </div>
-            <div className="grid grid-cols-6 mb-28">
+            <div className="grid grid-cols-7 lg:grid-cols-6 md:mb-28">
               <FeatureCard
                 title="Exclusive Investment Opportunities"
                 description="Discover Top-Tier, Secure Yield Options"
                 explanation="Access a curated selection of exclusive, secure investment opportunities with varying yield potentials, tailored to suit different risk profiles and financial goals."
               />
-              <div className="col-span-2 ml-20">
-                <Image src={exclusive} alt="mission" width={220} height={300} />
+              <div className="col-span-7 lg:col-span-2 flex justify-center items-center lg:ml-20 mt-10 lg:mt-0">
+                <Image
+                  src={exclusive}
+                  alt="mission"
+                  width={220}
+                  height={300}
+                  className="md:scale-[1] scale-[0.7]"
+                />
               </div>
             </div>
-            <div className="grid grid-cols-6 mb-28">
+            <div className="grid grid-cols-7 lg:grid-cols-6 md:mb-28">
               <FeatureCard
                 title="Seamless Cross-Chain bridging"
                 description="Effortlessly Connect Assets Across Blockchains"
                 explanation="Access a curated selection of exclusive, secure investment opportunities with varying yield potentials, tailored to suit different risk profiles and financial goals."
               />
-              <div className="col-span-2 ml-5">
+              <div className="col-span-7 lg:col-span-2 flex justify-center items-center lg:ml-5 mt-10 lg:mt-0">
                 <Image
                   src={crosschain}
                   alt="mission"
                   width={360}
                   height={300}
+                  className="md:scale-[1] scale-[0.7]"
                 />
               </div>
             </div>
@@ -106,24 +120,32 @@ const FeatureCard: React.FC<FeatureProps> = ({
   explanation,
 }) => {
   return (
-    <div className="#027EFF rounded-2xl col-span-4 translate-x-[-70px] border-[#027EFF] border">
+    <div className="#027EFF rounded-2xl col-span-7 lg:col-span-4 md:translate-x-[-70px] border-[#027EFF] border">
       <div
-        className={`pt-7 pb-7 px-5 rounded-t-2xl ${homepagestyles.featureCard}`}
+        className={`md:py-7 md:px-5 px-3 py-3 rounded-t-2xl ${homepagestyles.featureCard}`}
       >
-        <p className="text-[27px] uppercase gilroy-black-bold text-white">
+        <p className="text-sm md:text-md lg:text-[27px] uppercase gilroy-black-bold text-white">
           {title}
         </p>
-        <div className="flex">
-          <Image src={dot} alt="mission" width={20} height={20} />
-          <p className="text-white text-xl gilroy-regular ml-2">
+        <div className="flex items-center">
+          <Image
+            src={dot}
+            alt="mission"
+            width={20}
+            height={20}
+            className="scale-[0.7] md:scale-[1]"
+          />
+          <p className="text-white text-xs md:text-sm lg:text-xl gilroy-regular ml-2">
             {description}
           </p>
         </div>
       </div>
       <div
-        className={`pt-7 pb-7 px-5 rounded-b-2xl border-t border-t-[#027EFF]  ${homepagestyles.featureCard}`}
+        className={`md:py-7 md:px-5 px-3 py-3 rounded-b-2xl border-t border-t-[#027EFF]  ${homepagestyles.featureCard}`}
       >
-        <p className="text-white text-xl gilroy-regular">{explanation}</p>
+        <p className="text-white text-xs md:text-sm lg:text-xl gilroy-regular">
+          {explanation}
+        </p>
       </div>
     </div>
   );
