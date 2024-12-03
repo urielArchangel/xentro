540f67c7f0311264dcd70c8ab75f7bc1ddef138c
urielArchangel <itzdaglitch@gmail.com>
2024-12-03 15:14:41 +0100

    .

--- app/src/BE/helpers.ts
+++ app/src/BE/helpers.ts
@@ -10,10 +10,8 @@ import User from './DB/schemas/User';
 export const fetchAppData = async()=>{
    try{
     const res = await fetch("/api/admin/fetchApp",{next:{revalidate:0}})
-    const [app,error] = await res.json() as [IApp,any]
-    if(error){
-        throw new Error("An error occured")
-    }
+    const [app,_] = await res.json() as [IApp,any]
+ 
     return app
    }catch(err:any){}
 }
