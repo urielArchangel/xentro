d7e84ad8d6c1dad6fbf33ad7be37078fd82a22f0
urielArchangel <itzdaglitch@gmail.com>
2024-12-16 06:12:42 +0100

    dynamic-

--- app/admin/app/data/page.tsx
+++ app/admin/app/data/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Data from './Data'
 import { fetchAppData, fetchUsers } from '@/app/src/BE/helpers'
 export const dynamic = 'force-dynamic'; 
-export const revalidate=0
+export const revalidate=0 
 const page =async () => {
   const app  = await fetchAppData()
   const users = await fetchUsers()
